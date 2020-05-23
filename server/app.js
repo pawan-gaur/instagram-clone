cfc5d3e971049a1cce181097dc27eace44dc5b85
Pawan Gaur <pawangaur001@gmail.com>
2020-05-23 13:10:26 +0530

    3.13_3.14 : Commit code for creating post and post route && view all posts route

--- server/app.js
+++ server/app.js
@@ -4,11 +4,6 @@ const PORT = 5000;
 const mongoose = require("mongoose");
 const { MONGODB } = require("./keys");
 
-require("./models/user");
-
-app.use(express.json());
-app.use(require("./routes/auth"));
-
 mongoose.connect(MONGODB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -19,6 +14,13 @@ mongoose.connection.on("connected", () => {
 mongoose.connection.on("error", (error) => {
   console.log("error in connecting to mongo...!!", error);
 });
+
+require("./models/user");
+require("./models/post");
+
+app.use(express.json());
+app.use(require("./routes/auth"));
+app.use(require("./routes/post"));
 
 app.listen(PORT, () => {
   console.log("server is running on : ", PORT);
---
+++ server/routes/post.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const requireLogin = require("../middleware/requireLogin");
+const Post = mongoose.model("Post");
+
+router.post("/post", requireLogin, (req, res) => {
+  const { title, body } = req.body;
+  if (!title || !body) {
+    res.status(422).json({ error: "Please add the requied field" });
+  }
+
+  req.user.password = undefined;
+  const post = new Post({
+    title,
+    body,
+    postedBy: req.user,
+  });
+  post
+    .save()
+    .then((result) => {
+      res.json({ post: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/post", requireLogin, (req, res) => {
+  Post.find()
+    .populate("postedBy", "_id, name")
+    .then((posts) => {
+      res.json({ posts });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
