9430d8d5f9c21637453adaa70e2ed3c25beb634f
Pawan Gaur <pawangaur001@gmail.com>
2020-05-23 07:41:18 +0530

    2.6: commit code for creating user schema

--- server/app.js
+++ server/app.js
@@ -4,22 +4,7 @@ const PORT = 5000;
 const mongoose = require("mongoose");
 const { MONGODB } = require("./keys");
 
-// const customMiddeware = (req, res, next) => {
-//   console.log("middleware executed");
-//   next();
-// };
-
-//app.use(customMiddeware);
-
-// app.get("/", (req, res) => {
-//   console.log("Home");
-//   res.send("Hello World");
-// });
-
-// app.get("/about", customMiddeware, (req, res) => {
-//   console.log("about");
-//   res.send("about page");
-// });
+require("./models/user");
 
 mongoose.connect(MONGODB, {
   useNewUrlParser: true,
