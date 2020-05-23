e42aa0fb226f54e81355cf56e0c5e7bd6ccc9ccc
Pawan Gaur <pawangaur001@gmail.com>
2020-05-23 07:32:46 +0530

    2.5 : Commit code for setting up mongodb

--- server/app.js
+++ server/app.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
+const mongoose = require("mongoose");
+const { MONGODB } = require("./keys");
 
-const customMiddeware = (req, res, next) => {
-  console.log("middleware executed");
-  next();
-};
+// const customMiddeware = (req, res, next) => {
+//   console.log("middleware executed");
+//   next();
+// };
 
 //app.use(customMiddeware);
 
-app.get("/", (req, res) => {
-  console.log("Home");
-  res.send("Hello World");
-});
+// app.get("/", (req, res) => {
+//   console.log("Home");
+//   res.send("Hello World");
+// });
 
-app.get("/about", customMiddeware, (req, res) => {
-  console.log("about");
-  res.send("about page");
+// app.get("/about", customMiddeware, (req, res) => {
+//   console.log("about");
+//   res.send("about page");
+// });
+
+mongoose.connect(MONGODB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongo...");
+});
+mongoose.connection.on("error", (error) => {
+  console.log("error in connecting to mongo...!!", error);
 });
 
 app.listen(PORT, () => {
---
+++ server/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://<username>:<password>@<clustur>.mongodb.net/<database>?retryWrites=true&w=majority",
+};
