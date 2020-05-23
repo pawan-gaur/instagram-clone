e68fbb0ca620a00c7e6345f5987b42f5a0820cdd
Pawan Gaur <pawangaur001@gmail.com>
2020-05-23 08:18:48 +0530

    2.8 : commit code for posting user data to database

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const User = mongoose.model("User");
 
 router.get("/", (req, res) => {
   res.send("hello");
@@ -11,7 +14,30 @@ router.post("/singup", (req, res) => {
   if (!email || !password || !name) {
     return res.status(422).json({ error: "Please add the required value" });
   }
-  res.json({ message: "Successfully Saved" });
+  //res.json({ message: "Successfully Saved" });
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res.status(422).json({ error: "User already exists : ", email });
+      }
+      const user = new User({
+        email,
+        password,
+        name,
+      });
+
+      user
+        .save()
+        .then((user) => {
+          res.json({ message: "Successfully Saved" });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
